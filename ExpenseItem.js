58a789dc083200b568a0c9d1146515582c8cc28f
Shivang1102 <67555617+Shivang1102@users.noreply.github.com>
2023-03-16 19:54:39 +0530

    Adding css and how to use JavaScript inside JSX

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,10 +1,16 @@
+import './ExpenseItem.css';
 function ExpenseItem(){
-    return <div>
-        <div>Expense Tracker </div>
-        <div>
-            <h2> Food Rs 10</h2>
-            <h2>Petrol Rs 100</h2>
-            <h2>Movies Rs 200</h2>
+    const expenseTime = new Date(2021,2,24);
+    const expenseTitle ='Car Insurance';
+    const expenseAmount= 235.9;
+    const LocationofExpenditure= 'Manali';
+    return <div className='expense-item'>
+        <div >{expenseTime.toISOString()}</div>
+       
+        <div className='expense-item__description'>
+         <h2>{LocationofExpenditure}</h2>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
         </div>
     </div>
 }
