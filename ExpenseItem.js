c134636c7acb2a625cae87a194961c6616d2b81f
Shivang1102 <67555617+Shivang1102@users.noreply.github.com>
2023-03-16 16:26:51 +0530

    First Expense tracker expense file component

---
+++ ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem(){
+    return <div>
+        <div>Expense Tracker </div>
+        <div>
+            <h2> Food Rs 10</h2>
+            <h2>Petrol Rs 100</h2>
+            <h2>Movies Rs 200</h2>
+        </div>
+    </div>
+}
+
+export default ExpenseItem;
