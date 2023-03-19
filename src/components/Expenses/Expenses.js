d6a9b99c2dbd15e9687471fdbfa1138e0c665c0a
Shivang1102 <shivanggupta1102@gmail.com>
2023-03-19 15:05:25 +0530

    Fill char bar working

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,15 +4,18 @@ import Card from "../UI/Card";
 import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 
 const Expenses = (props) => {
 
   const [filteredYear, setFilteredYear] = useState("2020");
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
-
+  
+   
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
@@ -20,14 +23,16 @@ const Expenses = (props) => {
  
 
   return (
+    <div>
    <Card className='expenses'>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
+       <ExpensesChart  expenses={filteredExpenses}/>
        <ExpensesList items={filteredExpenses}/>
       </Card>
+      </div>
   );
 };
 
